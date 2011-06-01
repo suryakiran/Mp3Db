068cf379330673cbcb51a28a0aba634306f1a6d5
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2011-06-01 09:46:25 +0530

    Get current path from boost filesystem

--- TagRename/Src/DirView.cxx
+++ TagRename/Src/DirView.cxx
@@ -1,6 +1,13 @@
 #include <TagRename/DirView.hpp>
 #include <QtGui/QFileSystemModel>
 
+#include <iostream>
+using namespace std;
+
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/path.hpp>
+namespace fs = boost::filesystem;
+
 DirView::DirView(QWidget* p_parent)
   : QTreeView(p_parent)
 {
@@ -13,6 +20,10 @@ DirView::DirView(QWidget* p_parent)
   for (int i = 1; i < m_model->columnCount(); ++i) {
     setColumnHidden(i, true);
   }
+
+  QModelIndex idx (m_model->index(fs::current_path().c_str()));
+  selectionModel()->select(idx, QItemSelectionModel::Select);
+  setExpanded(idx, true);
 }
 
 DirView::~DirView()
