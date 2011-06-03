e4c3a01435bc9182987331910739bf745eccfb1b
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2011-06-03 23:37:39 +0530

    Added boost program_options library to parser command line arguments passed to the argument

--- TagRename/Src/Main.cxx
+++ TagRename/Src/Main.cxx
@@ -3,8 +3,32 @@
 
 #include <TagRename/MainWindow.hpp>
 
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/parsers.hpp>
+
+namespace po = boost::program_options;
+
+namespace {
+  void parseArgs (int argc, char** argv)
+  {
+    po::options_description desc ("Allowed Options");
+
+    desc.add_options()
+      ("help", "Display this message")
+      ("init-dir", po::value<std::string>(), "Initial directory to start with")
+      ;
+
+    po::variables_map vmap;
+    po::store(po::parse_command_line(argc, argv, desc), vmap);
+    po::notify(vmap);
+  }
+}
+
 int main (int argc, char** argv) 
 {
+  parseArgs (argc, argv);
+
   QApplication app (argc, argv);
 
   MainWindow *mainw = new MainWindow;
