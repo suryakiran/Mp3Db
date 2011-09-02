08d2ca26d19cd4ee6fd42854add9231d4d8404d8
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2011-09-02 22:55:19 +0530

    Modified mp3db.in script file to use Gdb/App perl modules in PerlModules submodule

--- TagRename/Src/MusicFileDisplayWidget.cxx
+++ TagRename/Src/MusicFileDisplayWidget.cxx
@@ -1,9 +1,36 @@
 #include <TagRename/MusicFileDisplayWidget.hpp>
 #include <TagRename/Mp3FileIterator.hpp>
-#include <QFileSystemModel>
+
+#include <QtCore/QTextCodec>
+#include <QtGui/QFileSystemModel>
+
+#include <taglib/fileref.h>
+#include <taglib/tag.h>
 
 #include <iostream>
 using namespace std;
+
+namespace {
+  QString toQString(TagLib::String s)
+  {
+    if (s.isLatin1()) //respect the tradition of writing data in system locale when no encoding is defined
+    {
+      QByteArray result;
+      result.resize(s.size());
+      for (int i=0;i<result.size();i++)
+        result[i]=s[i];
+
+      return QTextCodec::codecForLocale()->toUnicode(result);
+    }
+
+    QString result;
+    result.resize(s.size());
+    for (int i=0;i<result.size();i++)
+      result[i]=s[i];
+
+    return result;
+  }
+}
 
 MusicFileDisplayWidget::MusicFileDisplayWidget (QWidget* p_parent)
   : QTreeWidget(p_parent)
@@ -23,8 +50,18 @@ void MusicFileDisplayWidget::readDirectory (const QModelIndex& p_index)
     for (Mp3FileIterator iter (model->filePath(p_index).toStdWString()); 
         iter != endIter; ++iter)
     {
+      TagLib::FileName fileName (iter->path().filename().c_str());
+      TagLib::FileRef fileRef (fileName);
       QTreeWidgetItem* item = new QTreeWidgetItem(this);
-      item->setText (0, iter->path().filename().c_str());
+      TagLib::Tag* tag = fileRef.tag();
+      TagLib::AudioProperties* audioProperties = fileRef.audioProperties();
+      //TagLib::String album = tag->album();
+
+      item->setText (0, fileName);
+      if (fileRef.isNull())
+      {
+        item->setText (1, "Surya");
+      }
     }
   }
 
