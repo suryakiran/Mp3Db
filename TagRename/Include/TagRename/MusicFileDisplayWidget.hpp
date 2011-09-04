689e6ef45d188ddfebeeeca95d2afb11964be152
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2011-09-04 09:04:44 +0530

    Mp3 files and tags are read and populated

--- TagRename/Include/TagRename/MusicFileDisplayWidget.hpp
+++ TagRename/Include/TagRename/MusicFileDisplayWidget.hpp
@@ -2,10 +2,32 @@
 #define MusicFileDisplayWidget_hpp_INCLUDED
 
 #include <QtGui/QTreeWidget>
+#include <map>
 
 class MusicFileDisplayWidget : public QTreeWidget
 {
   Q_OBJECT
+
+  private:
+    typedef enum {
+      FileName = 0,
+      AlbumName,
+      TrackNumber,
+      TrackName,
+      ArtistNames,
+      Genre,
+      Composer,
+      BitRate,
+      Duration,
+    } Details;
+
+    typedef enum {
+      HiddenDataRole = Qt::UserRole + 1
+    } ItemRole;
+
+    typedef std::map<Details, QString> HeaderNameMap;
+    typedef HeaderNameMap::value_type HeaderNameMapValue;
+    HeaderNameMap m_headerNameMap;
 
   public:
     MusicFileDisplayWidget (QWidget* p_parent = 0);
--- TagRename/Src/MusicFileDisplayWidget.cxx
+++ TagRename/Src/MusicFileDisplayWidget.cxx
@@ -6,35 +6,42 @@
 
 #include <taglib/fileref.h>
 #include <taglib/tag.h>
+#include <taglib/id3v2tag.h>
+#include <taglib/mpegfile.h>
+
+#include <boost/assign/list_of.hpp>
+#include <boost/assign/std/map.hpp>
+#include <boost/foreach.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+using namespace boost::assign;
+
+namespace bt = boost::posix_time;
 
 #include <iostream>
 using namespace std;
 
-namespace {
-  QString toQString(TagLib::String s)
-  {
-    if (s.isLatin1()) //respect the tradition of writing data in system locale when no encoding is defined
-    {
-      QByteArray result;
-      result.resize(s.size());
-      for (int i=0;i<result.size();i++)
-        result[i]=s[i];
-
-      return QTextCodec::codecForLocale()->toUnicode(result);
-    }
-
-    QString result;
-    result.resize(s.size());
-    for (int i=0;i<result.size();i++)
-      result[i]=s[i];
-
-    return result;
-  }
-}
-
 MusicFileDisplayWidget::MusicFileDisplayWidget (QWidget* p_parent)
   : QTreeWidget(p_parent)
 {
+  QTreeWidgetItem* header = new QTreeWidgetItem();
+  insert (m_headerNameMap)
+    (FileName, "File Name")
+    (AlbumName, "Album Name")
+    (TrackNumber, "#")
+    (TrackName, "Track Name")
+    (ArtistNames, "Artist(s)")
+    (Genre, "Genre")
+    (Composer, "Composer")
+    (BitRate, "Bit Rate")
+    (Duration, "Duration")
+    ;
+
+  BOOST_FOREACH (HeaderNameMapValue himv, m_headerNameMap)
+  {
+    header->setText (himv.first, himv.second);
+  }
+  setHeaderItem (header);
+  setAlternatingRowColors (true);
 }
 
 MusicFileDisplayWidget::~MusicFileDisplayWidget()
@@ -50,18 +57,42 @@ void MusicFileDisplayWidget::readDirectory (const QModelIndex& p_index)
     for (Mp3FileIterator iter (model->filePath(p_index).toStdWString()); 
         iter != endIter; ++iter)
     {
-      TagLib::FileName fileName (iter->path().filename().c_str());
-      TagLib::FileRef fileRef (fileName);
+      fs::path p = iter->path();
+      TagLib::FileRef fileRef (p.string().c_str());
       QTreeWidgetItem* item = new QTreeWidgetItem(this);
       TagLib::Tag* tag = fileRef.tag();
       TagLib::AudioProperties* audioProperties = fileRef.audioProperties();
-      //TagLib::String album = tag->album();
+      TagLib::String album = tag->album();
 
-      item->setText (0, fileName);
-      if (fileRef.isNull())
+      int length (audioProperties->length());
+      bt::time_duration td = bt::seconds (length);
+
+      if (TagLib::MPEG::File* mpegFile = dynamic_cast<TagLib::MPEG::File*>(fileRef.file()))
       {
-        item->setText (1, "Surya");
+        const TagLib::ID3v2::Tag* id3v2Tag = mpegFile->ID3v2Tag();
+        if (id3v2Tag) 
+        {
+          const TagLib::ID3v2::FrameListMap& frameList = id3v2Tag->frameListMap();
+          TagLib::ID3v2::FrameListMap::ConstIterator iter = frameList.find("TCOM");
+          if (iter != frameList.end())
+          {
+            item->setText (Composer, TStringToQString(iter->second.front()->toString()));
+          }
+        }
       }
+      else
+      {
+      }
+
+      item->setText (FileName, p.filename().c_str());
+      item->setText (AlbumName, TStringToQString(tag->album()));
+      item->setText (TrackName, TStringToQString(tag->title()));
+      item->setText (Genre, TStringToQString(tag->genre()));
+      item->setText (ArtistNames, TStringToQString(tag->artist()));
+      item->setText (TrackNumber, QString("%1").arg(tag->track()));
+      item->setText (BitRate, QString("%1").arg(audioProperties->bitrate()));
+      item->setText (Duration, QString("%1").arg(bt::to_simple_string(td).c_str()));
+      item->setData (Duration, HiddenDataRole, length); 
     }
   }
 
