b33a04a63b538e971ca73185ea1a44ee6738843b
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2011-09-06 12:51:11 +0530

    Added style sheet

--- TagRename/Src/Main.cxx
+++ TagRename/Src/Main.cxx
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <fstream>
 
 #include <TagRename/MainWindow.hpp>
 
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/variables_map.hpp>
 #include <boost/program_options/parsers.hpp>
+#include <boost/algorithm/string/join.hpp>
 using namespace std;
 
 namespace po = boost::program_options;
 
 namespace {
+  string styleSheetFile;
+
   void parseArgs (int argc, char** argv)
   {
     po::options_description desc ("Allowed Options");
@@ -18,12 +23,39 @@ namespace {
     desc.add_options()
       ("help", "Display this message")
       ("init-dir", po::value<std::string>(), "Initial directory to start with")
+      ("style-sheet", po::value<std::string>(), "Style Sheet File")
       ;
 
     po::variables_map vmap;
     po::store(po::parse_command_line(argc, argv, desc), vmap);
     po::notify(vmap);
+
+    if (vmap.count("style-sheet"))
+    {
+      styleSheetFile = vmap["style-sheet"].as<string>();
+    }
   }
+
+  void setApplicationStyleSheet()
+  {
+    if (!styleSheetFile.empty())
+    {
+      fstream fin;
+      fin.open (styleSheetFile.c_str(), ios_base::in);
+
+      vector<string> lines;
+      string l;
+
+      while (getline (fin, l, '\n'))
+      {
+        lines.push_back (l);
+      }
+
+      fin.close();
+      qApp->setStyleSheet (boost::algorithm::join(lines, "\n").c_str());
+    }
+  }
+
 }
 
 int main (int argc, char** argv) 
@@ -31,6 +63,7 @@ int main (int argc, char** argv)
   parseArgs (argc, argv);
 
   QApplication app (argc, argv);
+  setApplicationStyleSheet();
 
   MainWindow *mainw = new MainWindow;
   mainw->show();
