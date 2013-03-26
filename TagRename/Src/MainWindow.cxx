e3fefabe25769fe6737913798237751fa80a5255
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2013-03-26 15:28:56 +0530

    Use stacked widget for documents and music

--- TagRename/Src/MainWindow.cxx
+++ TagRename/Src/MainWindow.cxx
@@ -12,14 +12,22 @@ MainWindow :: MainWindow(QWidget* p_parent)
   m_saveAndNext->setEnabled(false);
 
   connect(m_dirView->selectionModel(), SIGNAL(currentChanged(const QModelIndex&, const QModelIndex&)), 
-      m_musicFileDisplayWidget, SLOT(readDirectory(const QModelIndex&)));
+          m_musicFileDisplayWidget, SLOT(readDirectory(const QModelIndex&)));
+  connect(m_musicFileDisplayWidget, SIGNAL(hasItemSelection(bool)),
+          m_musicFilePropertiesFrame, SLOT(setVisible(bool)));
+  connect(m_musicFileDisplayWidget, SIGNAL(hasItemSelection(bool)),
+          m_saveAndPrev, SLOT(setEnabled(bool)));
+  connect(m_musicFileDisplayWidget, SIGNAL(hasItemSelection(bool)),
+          m_saveAndNext, SLOT(setEnabled(bool)));
+  connect(m_musicFileDisplayWidget->selectionModel(),
+          SIGNAL(currentChanged(const QModelIndex&, const QModelIndex&)),
+          m_musicFilePropertiesWidget,
+          SLOT(updateDetailsOfSelectedFile(const QModelIndex&)));
+  connect(m_saveAndNext, SIGNAL(clicked()),
+          m_musicFileDisplayWidget, SLOT(saveCurrentAndGotoNextItem()));
+  connect(m_saveAndPrev, SIGNAL(clicked()),
+          m_musicFileDisplayWidget, SLOT(saveCurrentAndGotoPrevItem()));
 
-  connect(m_musicFileDisplayWidget, SIGNAL(hasItemSelection(bool)), m_musicFilePropertiesFrame, SLOT(setVisible(bool)));
-  connect(m_musicFileDisplayWidget, SIGNAL(hasItemSelection(bool)), m_saveAndPrev, SLOT(setEnabled(bool)));
-  connect(m_musicFileDisplayWidget, SIGNAL(hasItemSelection(bool)), m_saveAndNext, SLOT(setEnabled(bool)));
-  connect(m_musicFileDisplayWidget->selectionModel(), SIGNAL(currentChanged(const QModelIndex&, const QModelIndex&)),
-          m_musicFilePropertiesWidget, SLOT(updateDetailsOfSelectedFile(const QModelIndex&)));
-
-  connect(m_saveAndNext, SIGNAL(clicked()), m_musicFileDisplayWidget, SLOT(saveCurrentAndGotoNextItem()));
-  connect(m_saveAndPrev, SIGNAL(clicked()), m_musicFileDisplayWidget, SLOT(saveCurrentAndGotoPrevItem()));
+  buttonGroup->setId(musicButton, 0);
+  buttonGroup->setId(docButton, 1);
 }
